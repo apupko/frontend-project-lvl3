a562685f59a4fd16fec88654565fe71213155d5b
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-25 09:09:29 +0600

    Create

---
+++ __tests__/validator.test.js
@@ -0,0 +1,37 @@
+import nock from 'nock';
+import path from 'path';
+import isValidRssUrl from '../src/validator.js';
+
+nock.disableNetConnect();
+
+const host = 'https://hexlet.ru';
+const validRssUrl = '/valid.rss';
+const invalidRssUrl = '/invalid.rss';
+const wrongUrl = ':wrongurl.com';
+const nonExistentUrl = '/nonexisten.rss';
+
+const pathToValidRss = path.resolve(__dirname, '../__fixture__', validRssUrl);
+const pathToInvalidRss = path.resolve(__dirname, '../__fixture__', invalidRssUrl);
+
+nock(host)
+  .get(validRssUrl)
+  .replyWithFile(200, pathToValidRss, {
+    'Content-Type': 'application/rss+xml',
+  })
+  .get(invalidRssUrl)
+  .replyWithFile(200, pathToInvalidRss, {
+    'Content-Type': 'application/rss+xml',
+  })
+  .get(nonExistentUrl)
+  .reply(404);
+
+const urls = [
+  ['valid url', `${host}${validRssUrl}`, true],
+  ['invalid url', `${host}${invalidRssUrl}`, false],
+  ['non existen url', `${host}${nonExistentUrl}`, false],
+  ['wrong url', wrongUrl, false],
+];
+
+test.each(urls)('case %#: %s', (caseName, url, expected) => {
+  expect(isValidRssUrl(url)).toBe(expected);
+});

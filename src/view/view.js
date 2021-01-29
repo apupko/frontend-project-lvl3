8c7ef47b15857a1c27b9d3d4c80b43517233db29
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-29 14:27:54 +0600

    Add imports

--- src/view/view.js
+++ src/view/view.js
@@ -1,4 +1,6 @@
 import renderForm from './form';
+import renderFeeds from './feeds';
+import renderPosts from './posts';
 
 function init() {
   const { body } = document;
@@ -7,6 +9,8 @@ function init() {
   this.form = body.querySelector('.rss-form');
   this.example = body.querySelector('#example');
   this.feedback = body.querySelector('.feedback');
+  this.feeds = body.querySelector('.feeds');
+  this.posts = body.querySelector('.posts');
 }
 
 function View() {

6fe00014fd05cfa0ad3f0be1caff01c3813a6e7b
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-29 13:39:17 +0600

    Refactoring

--- src/view/view.js
+++ src/view/view.js
@@ -1,3 +1,5 @@
+import renderForm from './form';
+
 function init() {
   const { body } = document;
   this.title = body.querySelector('#mainTitle');
@@ -7,49 +9,11 @@ function init() {
   this.feedback = body.querySelector('.feedback');
 }
 
-function setFormToFillingView() {
-  const { form, feedback } = this;
-  form.elements.url.classList.remove('is-invalid');
-  form.elements.url.classList.add('is-valid');
-  form.elements.submitBtn.classList.remove('disabled');
-  feedback.classList.remove('text-danger');
-}
-
-const setFormToSendingView = () => {
-  const { form, feedback } = view;
-  form.elements.submitBtn.classList.add('disabled');
-  feedback.classList.remove('text-danger');
-};
-
-const setFormToFailedView = (view, errors) => {
-  const { form, feedback } = view;
-  form.elements.url.classList.add('is-invalid');
-  form.elements.url.classList.remove('is-valid');
-  form.elements.submitBtn.classList.remove('disabled');
-  feedback.textContent = errors.join(' ');
-  feedback.classList.add('text-danger');
-};
-
-const mappingFormState = {
-  filling: setFormToFillingView,
-  sending: setFormToSendingView,
-  failed: setFormToFailedView,
-};
-
-function renderForm({ feedForm }) {
-  const { state, fields } = feedForm;
-  const updateForm = mappingFormState[state];
-  if (!updateOperation) throw Error('Unknown form status!!!');
-  updateForm(this);
-}
-
-const render = (state) => {
-
-};
-
 function View() {
   this.init = init;
-  this.renderForm = renderForm;
+  this.renderForm = renderForm(this);
+  this.renderFeeds = renderFeeds(this);
+  this.renderPosts = renderPosts(this);
 }
 
 export default View;

d9818f080a1f96c7a46ab0207ec7f2ea31e5b1c2
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-29 13:36:37 +0600

    Change structure for parser result

--- __tests__/parser.test.js
+++ __tests__/parser.test.js
@@ -2,14 +2,36 @@ import { readFixtureFile } from '../__fixtures__/utils.js';
 import rss from '../src/parser';
 
 const expected = {
-  title: 'RSS feed',
-  description: 'Simple rss feed',
+  feed: {
+    title: 'RSS feed',
+    description: 'Simple rss feed',
+    link: '#',
+    id: 1,
+  },
   posts: [
-    { title: 'First post', description: 'First post description', link: '#' },
-    { title: 'Second post', description: 'Second post description', link: '#' },
+    {
+      title: 'First post',
+      description: 'First post description',
+      link: '#',
+      id: 1,
+      feedId: 1,
+    },
+    {
+      title: 'Second post',
+      description: 'Second post description',
+      link: '#',
+      id: 1,
+      feedId: 1,
+    },
   ],
 };
 
-test('parse valid rss', () => readFixtureFile('test.rss')
-  .then((data) => rss.parse(data))
-  .then((obj) => expect(obj).toEqual(expected)));
+const rssDataFile = [
+  ['valid', 'test.rss', expected],
+  ['invalid', 'index.html', {}],
+];
+
+test.each(rssDataFile)('parse %s rss', (caseName, file, expectedResult) => (
+  readFixtureFile(file)
+    .then((data) => rss.parse(data, () => 1))
+    .then((obj) => expect(obj).toEqual(expectedResult))));
--- src/parser.js
+++ src/parser.js
@@ -1,20 +1,39 @@
 import { map } from 'lodash';
 
-const parseItem = (item) => {
+const parseItem = (item, feedId, generateId) => {
   const title = item.querySelector('title').textContent;
   const description = item.querySelector('description').textContent;
   const link = item.querySelector('link').textContent;
-  return { title, description, link };
+  return {
+    title,
+    description,
+    link,
+    id: generateId(),
+    feedId,
+  };
 };
 
-const parse = (data) => {
+const parseFeed = (channel, generateId) => {
+  const title = channel.querySelector('title').textContent;
+  const description = channel.querySelector('description').textContent;
+  const link = channel.querySelector('link').textContent;
+  return {
+    title,
+    description,
+    link,
+    id: generateId(),
+  };
+};
+
+const parse = (data, generateId) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'application/xml');
   const channel = doc.querySelector('channel');
-  const title = channel.querySelector('title').textContent;
-  const description = channel.querySelector('description').textContent;
-  const posts = map(channel.getElementsByTagName('item'), parseItem);
-  return { title, description, posts };
+  if (!channel) return {};
+  const feed = parseFeed(channel, generateId);
+  const posts = map(channel.getElementsByTagName('item'),
+    (item) => parseItem(item, feed.id, generateId));
+  return { feed, posts };
 };
 
 export default { parse };

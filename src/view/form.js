1c4ccbab467d25271e720ed0cbd7b1db898bde3c
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-29 14:28:37 +0600

    Fix renders

--- src/view/form.js
+++ src/view/form.js
@@ -1,20 +1,14 @@
-const setValid = (element) => {
-  element.classList.remove('is-invalid');
-  element.classList.add('is-valid');
-};
+const removeInvalid = (element) => element.classList.remove('is-invalid');
+const setInvalid = (element) => element.classList.add('is-invalid');
 
-const setInvalid = (element) => {
-  element.classList.remove('is-valid');
-  element.classList.add('is-invalid');
-};
+const enableElement = (element) => element.setAttribute('disabled', false);
+const disableElement = (element) => element.setAttribute('disabled', true);
 
-const enableElement = (element) => element.classList.remove('disabled');
-const disableElement = (element) => element.classList.add('disabled');
-
-const renderFeedback = (view, message, { isError = false }) => {
+const renderFeedback = (view, message, options = { error: false }) => {
   const { feedback } = view;
   feedback.textContent = message;
-  if (isError) {
+  const { error } = options;
+  if (error) {
     feedback.classList.add('text-danger');
     return;
   }
@@ -24,15 +18,17 @@ const renderFeedback = (view, message, { isError = false }) => {
 const renderFormToFilling = (view) => {
   const { form } = view;
   const { url, submitBtn } = form.elements;
-  setValid(url);
+  removeInvalid(url);
   enableElement(submitBtn);
+  enableElement(url);
 };
 
 const renderFormToSending = (view) => {
   const { form } = view;
   const { url, submitBtn } = form.elements;
-  setValid(url);
+  removeInvalid(url);
   disableElement(submitBtn);
+  disableElement(url);
 };
 
 const renderFormToFailed = (view, errors) => {
@@ -40,16 +36,18 @@ const renderFormToFailed = (view, errors) => {
   const { url, submitBtn } = form.elements;
   setInvalid(url);
   enableElement(submitBtn);
+  enableElement(url);
   const msg = errors.join(' ');
-  renderFeedback(view, msg, { isError: true });
+  renderFeedback(view, msg, { error: true });
 };
 
 const renderFormToLoaded = (view) => {
   const { form } = view;
   const { url, submitBtn } = form.elements;
-  setValid(url);
+  removeInvalid(url);
   url.textContent = '';
   enableElement(submitBtn);
+  enableElement(url);
   const msg = 'Loaded';
   renderFeedback(view, msg);
 };

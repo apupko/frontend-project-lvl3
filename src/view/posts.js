84d5599eb6572dba75e458611cb9c40214147e27
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-31 20:41:49 +0600

    Create

--- src/view/posts.js
+++ src/view/posts.js
@@ -1,3 +1,13 @@
-export default (view) => ({ posts }) => {
+import { createHeader, createItem, createList } from './utils.js';
 
+export default (view) => ({ posts }) => {
+  const { posts: postsDiv } = view;
+  if (posts.length === 0) {
+    postsDiv.innerHTML = '';
+    return;
+  }
+  const header = createHeader('Posts');
+  const items = posts.map(createItem);
+  const list = createList(items);
+  postsDiv.append(header, list);
 };

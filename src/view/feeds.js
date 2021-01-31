b261b6eea6c591367cdbde302e7c1ad502b83994
Alexandr Pupko <pupko.alexander@gmail.com>
2021-01-31 20:42:23 +0600

    Refactoring

--- src/view/feeds.js
+++ src/view/feeds.js
@@ -1,30 +1,4 @@
-const buildHeader = (title) => {
-  const header = document.createElement('h2');
-  header.textContent = title;
-  return header;
-};
-
-const buildItem = (feed) => {
-  const title = document.createElement('h3');
-  const description = document.createElement('p');
-  const item = document.createElement('li');
-
-  title.textContent = feed.title;
-  description.textContent = feed.description;
-  item.classList.add('list-group-item');
-  item.append(title, description);
-
-  return item;
-};
-
-const buildList = (feeds) => {
-  const list = document.createElement('ul');
-  const items = feeds.map(buildItem);
-  list.classList.add('list-group', 'mb-5');
-  items.forEach((item) => list.append(item));
-
-  return list;
-};
+import { createHeader, createItem, createList } from './utils.js';
 
 export default (view) => ({ feeds }) => {
   const { feeds: feedsDiv } = view;
@@ -32,7 +6,8 @@ export default (view) => ({ feeds }) => {
     feedsDiv.innerHTML = '';
     return;
   }
-  const header = buildHeader('Feeds');
-  const list = buildList(feeds);
+  const header = createHeader('Feeds');
+  const items = feeds.map(createItem);
+  const list = createList(items);
   feedsDiv.append(header, list);
 };

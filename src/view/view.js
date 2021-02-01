94db4a4721c3afd057a5a8038aea053079eb41a3
Alexandr Pupko <pupko.alexander@gmail.com>
2021-02-01 21:58:32 +0600

    Add i18next init

--- src/view/view.js
+++ src/view/view.js
@@ -1,4 +1,6 @@
 import onChange from 'on-change';
+import i18next from 'i18next';
+import en from './locales/en.js';
 import renderForm from './form';
 import renderFeeds from './feeds';
 import renderPosts from './posts';
@@ -18,15 +20,35 @@ const watchingFunction = (view) => function func(path) {
   render(this);
 };
 
+const initStaticContents = () => {
+  const title = document.body.querySelector('#mainTitle');
+  const description = document.body.querySelector('#mainDesc');
+  const example = document.body.querySelector('#example');
+  const form = document.body.querySelector('.rss-form');
+  const { submitBtn, url } = form.elements;
+
+  title.textContent = i18next.t('mainPage.title');
+  description.textContent = i18next.t('mainPage.description');
+  example.textContent = i18next.t('mainPage.example');
+  submitBtn.textContent = i18next.t('form.buttonLabel');
+  url.setAttribute('placeholder', i18next.t('form.inputPlaceholder'));
+};
+
 function init(state) {
-  const { body } = document;
-  this.title = body.querySelector('#mainTitle');
-  this.description = body.querySelector('#mainDesc');
-  this.form = body.querySelector('.rss-form');
-  this.example = body.querySelector('#example');
-  this.feedback = body.querySelector('.feedback');
-  this.feeds = body.querySelector('.feeds');
-  this.posts = body.querySelector('.posts');
+  this.form = document.body.querySelector('.rss-form');
+  this.feedback = document.body.querySelector('.feedback');
+  this.feeds = document.body.querySelector('.feeds');
+  this.posts = document.body.querySelector('.posts');
+
+  i18next.init({
+    lng: 'en',
+    debug: false,
+    resources: {
+      en,
+    },
+  })
+    .then(() => initStaticContents())
+    .catch((err) => console.log(err));
 
   return onChange(state, watchingFunction(this));
 }

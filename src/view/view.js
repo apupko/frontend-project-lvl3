842266a68edc4c42765520631d21b95d26b064e0
Alexandr Pupko <pupko.alexander@gmail.com>
2021-02-01 11:21:51 +0600

    Add watching function

--- src/view/view.js
+++ src/view/view.js
@@ -1,8 +1,24 @@
+import onChange from 'on-change';
 import renderForm from './form';
 import renderFeeds from './feeds';
 import renderPosts from './posts';
 
-function init() {
+const watchingFunction = (view) => function (path) {
+  console.log(`path - ${path}`);
+  const mappingPathToRender = {
+    'feedForm.state': renderForm(view),
+    errors: renderForm(view),
+    feeds: renderFeeds(view),
+    posts: renderPosts(view),
+  };
+  const render = mappingPathToRender[path];
+  if (!render) {
+    throw new Error('Unknown app state!!!');
+  }
+  render(this);
+};
+
+function init(state) {
   const { body } = document;
   this.title = body.querySelector('#mainTitle');
   this.description = body.querySelector('#mainDesc');
@@ -11,13 +27,12 @@ function init() {
   this.feedback = body.querySelector('.feedback');
   this.feeds = body.querySelector('.feeds');
   this.posts = body.querySelector('.posts');
+
+  return onChange(state, watchingFunction(this));
 }
 
 function View() {
   this.init = init;
-  this.renderForm = renderForm(this);
-  this.renderFeeds = renderFeeds(this);
-  this.renderPosts = renderPosts(this);
 }
 
 export default View;

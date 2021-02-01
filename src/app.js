336bd8164051b36a25af6a19ebd8aa44573fc911
Alexandr Pupko <pupko.alexander@gmail.com>
2021-02-01 11:23:12 +0600

    Add app logic

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
-import view from './view.js';
-import rssParser from './parser.js';
+import axios from 'axios';
+import _ from 'lodash';
+import View from './view/view.js';
+import { validateUrl, validateResponse } from './validator.js';
+import RSSParser from './parser.js';
 
 export default () => {
-  const state = {
+  const initState = {
     feedForm: {
-      state: 'filing',
+      state: 'filling',
       fields: {
         url: '',
       },
@@ -14,7 +17,33 @@ export default () => {
     posts: [],
   };
 
-  view.init();
-  state.feedForm.state = 'failed';
-  view.renderForm(state);
+  const view = new View();
+  const state = view.init(initState);
+
+  const { form } = view;
+
+  const formSubmitHahdler = (event) => {
+    event.preventDefault();
+    const { value } = event.target.elements.url;
+    const feedsUrls = state.feeds.map((feed) => _.get(feed, 'link'));
+    validateUrl(value, feedsUrls)
+      .then((validUrl) => {
+        state.feedForm.state = 'sending';
+        return axios.get(validUrl);
+      })
+      .then((response) => validateResponse(response))
+      .then(({ data }) => RSSParser.parse(data.contents))
+      .then(({ feeds, posts }) => {
+        state.feedForm.state = 'finished';
+        state.feeds = [...feeds];
+        state.posts = [...posts];
+        state.feedForm.state = 'filling';
+      })
+      .catch((err) => {
+        state.feedForm.state = 'failed';
+        state.errors = [...err.errors];
+      });
+  };
+
+  form.addEventListener('submit', formSubmitHahdler);
 };
